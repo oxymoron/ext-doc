199e646e48a56e23a121dc85851d5e0e3515ac1b
oxxxymoron <oxxxymoron@4a32c7fa-a550-11dd-a8c6-677c70d10fe4>
2010-07-03 13:05:17 +0000

    Test cases for CLI, creating jar

--- extdoc-cli/src/main/java/extdoc/Main.java
+++ extdoc-cli/src/main/java/extdoc/Main.java
@@ -5,6 +5,8 @@ import extdoc.jsdoc.processor.FileProcessor;
 import extdoc.jsdoc.processor.FileProcessorImpl;
 import org.apache.commons.cli.*;
 
+import java.text.MessageFormat;
+
 /**
  * User: Andrey Zubkov
  * Date: 25.10.2008
@@ -14,18 +16,19 @@ import org.apache.commons.cli.*;
 public class Main {
 
     private static Options options = null;
+    private static final String WRONG_CLI_ARGUMENTS = "Wrong command line arguments: {0}";
+    private static final String HELP_MESSAGE = "java -jar ext-doc.jar [-p project] -o output [-s source1 -s source2 ...]";
 
     private static void wrongCli(String msg){
-        System.err.println("Wrong command line arguments: "+ msg);
+        System.err.println(MessageFormat.format(WRONG_CLI_ARGUMENTS, msg));
         showHelp();
     }
 
     private static void showHelp(){
         HelpFormatter formatter = new HelpFormatter();
-        formatter.printHelp( "java -jar ext-doc.jar [-p project] -o output [-s source1 -s source2 ...]", options);
+        formatter.printHelp(HELP_MESSAGE, options);
     }
 
-    @SuppressWarnings("static-access")
     public static void main(String[] args) {
 
         prepareOptions();
@@ -43,6 +46,9 @@ public class Main {
             CommandLine cmd = parser.parse( options, args);
             if(cmd.hasOption("project")||cmd.hasOption("source")){
 
+                if (cmd.hasOption("quiet") && cmd.hasOption("verbose")){
+                    throw new WrongCliException("Processing cannot be quiet and verbose at the same time");
+                }
                 if(cmd.hasOption("quiet")){
                     processor.setQuiet();
                 }else if (cmd.hasOption("verbose")){
@@ -63,6 +69,7 @@ public class Main {
         }
     }
 
+    @SuppressWarnings("static-access")
     public static void prepareOptions() {
         
         options = new Options();
--- extdoc-cli/src/test/java/extdoc/MainTest.java
+++ extdoc-cli/src/test/java/extdoc/MainTest.java
@@ -2,9 +2,11 @@ package extdoc;
 
 import extdoc.exceptions.WrongCliException;
 import extdoc.jsdoc.processor.FileProcessor;
+import org.junit.BeforeClass;
 import org.junit.Test;
+
 import static org.easymock.EasyMock.*;
-import static org.junit.Assert.*;
+import static org.junit.Assert.fail;
 
 /**
  * Created by IntelliJ IDEA.
@@ -15,11 +17,54 @@ import static org.junit.Assert.*;
  */
 public class MainTest {
 
-    @Test (expected = WrongCliException.class)
-    public void firstTest() throws WrongCliException {
-        FileProcessor fileProcessor = createMock(FileProcessor.class);
+    // all the following strings are incorrect
+    private static final String[] wrongCommands = {
+        "test",
+        "-p projectFile",
+        "-o output",
+        "-p projectFile -o outputFolder -t templateFile -q -verbose",
+        "-s source"
+    };
+
+    @BeforeClass
+    public static void setUp(){
         Main.prepareOptions();
-        Main.processCli(fileProcessor, new String[]{"test"});
+    }
+
+    @Test
+    public void wrongParamsTest(){
+        FileProcessor fileProcessor = createMock(FileProcessor.class);
+        for (String s : wrongCommands){
+            try {
+                Main.processCli(fileProcessor, s.split(" "));
+            } catch (WrongCliException e) {
+                continue;
+            }
+            // If exception not thrown then test fails.
+            // Exception must be thrown for every wrong command.
+            fail();
+        }
+    }
+
+    @Test
+    public void allTest() throws WrongCliException {
+        FileProcessor fileProcessor = createMock(FileProcessor.class);
+        fileProcessor.process("projectFile", null);
+        fileProcessor.setQuiet();
+        fileProcessor.saveToFolder("outputFolder", "templateFile");
+        replay(fileProcessor);        
+        Main.processCli(fileProcessor, "-p projectFile -o outputFolder -t templateFile -q".split(" "));
+        verify(fileProcessor);
+    }
+
+    @Test
+    public void sourceTest() throws WrongCliException {
+        FileProcessor fileProcessor = createMock(FileProcessor.class);
+        fileProcessor.process(isNull(String.class), aryEq(new String[]{"source1", "source2"}));        
+        fileProcessor.saveToFolder("outputFolder", "templateFile");
+        replay(fileProcessor);
+        Main.processCli(fileProcessor, "-s source1 -s source2 -o outputFolder -t templateFile".split(" "));
+        verify(fileProcessor);
     }
 
 }
